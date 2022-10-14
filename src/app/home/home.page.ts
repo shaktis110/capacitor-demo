d569a85306fe23fbfa834097e77de2ababc8dd0e
Unknown user <38592947+shaktis110@users.noreply.github.com>
2022-10-14 21:09:52 +0530

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FirebaseX } from '@awesome-cordova-plugins/firebase-x/ngx';
 import { AlertController,Platform } from '@ionic/angular';
+import { StatusBar } from '@awesome-cordova-plugins/status-bar/ngx';
+declare var NavigationBar :any;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,8 +10,18 @@ import { AlertController,Platform } from '@ionic/angular';
 })
 export class HomePage {
 tokan;
-  constructor(private firebasex: FirebaseX,private alert: AlertController,private platform: Platform) {
+  constructor(private firebasex: FirebaseX,private alert: AlertController,private platform: Platform,private statusBar: StatusBar) {
       this.platform.ready().then(() =>{
+        
+          //For bottom statusbar
+  // second argument of function role lightNavigationBar Change the color of the buttons in
+  // the navigation bar to black, use in light colors of the navigation bar (Android 8.0 or higher).
+  NavigationBar.backgroundColorByHexString('#00FFFF',false);
+
+ // for top status bar
+ this.statusBar.backgroundColorByHexString('#00FF00');
+        
+        
       this.firebasex.getToken()
       .then(tokan => {console.log(`The token is ${tokan}`);
       this.tokan = tokan;
